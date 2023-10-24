022343bcab6b98e855abfd50718fd531fa474edb
Tomáš Duchoslav <130918563+GosutoDev@users.noreply.github.com>
2023-10-24 13:29:01 +0200

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Tomáš Duchoslav on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
